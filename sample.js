f92dbf18a0e3d34f0ddfd1e32180e9bdb5138432
ajmalgithubm <ajmalm76774@gmail.com>
2023-08-03 19:40:41 +0530

    module is added

--- sample.js
+++ sample.js
@@ -1,23 +1,2 @@
-
-function longTask(milliSecond){
-    dt = new Date();
-    while( (new Date()-dt) < milliSecond){
-       // console.log(new Date() -dt)
-    }
-    console.log("end")
-}
-
-function showEnd(){
-    console.log("End..")
-}
-
-
-var hello = (callback) => {
-    callback("Ajmal")
-}
-
-var hey = (data) => {
-    console.log("Hey",data)
-}
-
-hello(hey)
+const m = require('./index');
+m.hello()
