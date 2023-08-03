303771123c2f3315fdca27b06e6f4955f0bb894f
ajmalgithubm <ajmalm76774@gmail.com>
2023-08-03 22:04:42 +0530

    final commit

--- sampleServer.js
+++ sampleServer.js
@@ -2,9 +2,19 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer((req, res) => {
-    fs.readFile('sample.html', function(err, data){
-        res.writeHead(200, {'content-type':'text/html'});
-        res.write(data);
-        res.end();
-    })
-}).listen(3000)
+    if(req.url === '/'){
+        fs.readFile('sample.html', function (err, data) {
+            res.writeHead(200, { 'content-type': 'text/html' });
+            res.write(data);
+            res.end();
+        })
+    }else if(req.url === '/about'){
+        res.write("About Page")
+        res.end()
+    }else{
+        res.write("This is Error")
+        res.end()
+    }
+}).listen(3000, () => {
+    console.log("Server is Running...")
+})
