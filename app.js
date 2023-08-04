600710037fb992a2a0f863f46ec2ab202a19c317
ajmalgithubm <ajmalm76774@gmail.com>
2023-08-04 22:43:00 +0530

    mongodb is added

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
 app.use('/', welcomeRouter)
-app.use('/home', homeRouter)
 
 
 // app.use('/users', usersRouter);
--- routes/Welcome.js
+++ routes/Welcome.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const path = require('path')
 const router = express.Router();
+const url = 'mongodb://127.0.0.1:27017';
+const MongoClient = require('mongodb').MongoClient
+
 router.get('/', (req, res) => {
-    console.log("Base Root Is called")
-    res.render('Welcome')
+   res.render('welcome')
 })
 
-router.get('/home', (req,res) => {
-    console.log("Home Page is Called");
-    res.send("Home Page")
+router.post('/submit', (req, res) => {
+    console.log(req.body)
+    const email = req.body.emailaddress;
+    const password = req.body.password;
+    MongoClient.connect(url).then((client) => {
+        const db = client.db('project');
+        const userCollection = db.collection('user');
+        userCollection.insertOne({email,password}).then(() => {
+            res.send("added To database");
+            client.close()
+        })
+    })
+
 })
 
-router.get('/profile', (req,res) => {
-    console.log("Profile Page is Called");
-    res.send("Profile Page")
-})
 
-router.get('/about', (req,res) => {
-    console.log("About page is  called");
-    res.send("about Page")
-})
 module.exports = router;
